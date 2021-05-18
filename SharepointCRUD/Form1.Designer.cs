7aeacf00c33b1e2c6be6f60cda2167a0eeb3c9ac
shrushti somshette <shrushti.somshette@nitorinfotech.in>
2021-05-18 23:23:50 +0530

    test

--- SharepointCRUD/Form1.Designer.cs
+++ SharepointCRUD/Form1.Designer.cs
@@ -36,6 +36,18 @@
             this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.button19 = new System.Windows.Forms.Button();
+            this.button20 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -118,11 +130,143 @@
             this.button9.UseVisualStyleBackColor = true;
             this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(448, 109);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(160, 56);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "AddListDescription";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(650, 109);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(136, 56);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "Add list Title";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(650, 25);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(136, 48);
+            this.button11.TabIndex = 11;
+            this.button11.Text = "EnableVersioning";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(448, 197);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(160, 45);
+            this.button12.TabIndex = 12;
+            this.button12.Text = "Enable Minor Versioning";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(650, 197);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(136, 45);
+            this.button13.TabIndex = 13;
+            this.button13.Text = "Enable Major Versioning";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(278, 276);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(123, 52);
+            this.button14.TabIndex = 14;
+            this.button14.Text = "Enable force Checkout";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(448, 276);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(160, 52);
+            this.button15.TabIndex = 15;
+            this.button15.Text = "Folder CReation In List";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(650, 276);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(136, 52);
+            this.button16.TabIndex = 16;
+            this.button16.Text = "Enable Quick launch";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(81, 353);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(112, 60);
+            this.button17.TabIndex = 17;
+            this.button17.Text = "Enable Content Type";
+            this.button17.UseVisualStyleBackColor = true;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // button18
+            // 
+            this.button18.Location = new System.Drawing.Point(278, 353);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(123, 60);
+            this.button18.TabIndex = 18;
+            this.button18.Text = "Add Content Type";
+            this.button18.UseVisualStyleBackColor = true;
+            this.button18.Click += new System.EventHandler(this.button18_Click);
+            // 
+            // button19
+            // 
+            this.button19.Location = new System.Drawing.Point(448, 353);
+            this.button19.Name = "button19";
+            this.button19.Size = new System.Drawing.Size(160, 60);
+            this.button19.TabIndex = 19;
+            this.button19.Text = "Delete Content type from list";
+            this.button19.UseVisualStyleBackColor = true;
+            this.button19.Click += new System.EventHandler(this.button19_Click);
+            // 
+            // button20
+            // 
+            this.button20.Location = new System.Drawing.Point(650, 353);
+            this.button20.Name = "button20";
+            this.button20.Size = new System.Drawing.Size(136, 60);
+            this.button20.TabIndex = 20;
+            this.button20.Text = "Delete All site Contents";
+            this.button20.UseVisualStyleBackColor = true;
+            this.button20.Click += new System.EventHandler(this.button20_Click_1);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(823, 405);
+            this.ClientSize = new System.Drawing.Size(884, 532);
+            this.Controls.Add(this.button20);
+            this.Controls.Add(this.button19);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button8);
             this.Controls.Add(this.button9);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button6);
@@ -147,6 +291,18 @@
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.Button button19;
+        private System.Windows.Forms.Button button20;
     }
 }
 
--- SharepointCRUD/Form1.cs
+++ SharepointCRUD/Form1.cs
@@ -225,6 +225,330 @@ namespace SharepointCRUD
             }
         }
 
+        /// <summary>
+        /// method to add discription
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button8_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Add List Description
+                using (ClientContext clientContext = new ClientContext("http://vm-021-tzc02/sites/CSOMSite/"))
+                {
+                    Web web = clientContext.Web;
+                    List list = web.Lists.GetByTitle("Employee");
+                    list.Description = "Added Discription";
+                    list.Update();
+                    clientContext.ExecuteQuery();
+                }
+                MessageBox.Show("List Description Added");
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+
+        /// <summary>
+        /// Method to update title
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button10_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Add List Description
+                using (ClientContext clientContext = new ClientContext("http://vm-021-tzc02/sites/CSOMSite/"))
+                {
+                    Web web = clientContext.Web;
+                    List list = web.Lists.GetByTitle("AddedTitle");
+                    list.Title = "Employee";
+                    list.Update();
+                    clientContext.ExecuteQuery();
+                }
+                MessageBox.Show("List Title Added");
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+        /// <summary>
+        /// List versioning enabled
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button11_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Add List Description
+                using (ClientContext clientContext = new ClientContext("http://vm-021-tzc02/sites/CSOMSite/"))
+                {
+                    Web web = clientContext.Web;
+                    List list = web.Lists.GetByTitle("Employee");
+                    list.EnableVersioning = true;
+                    list.Update();
+                    clientContext.ExecuteQuery();
+                }
+                MessageBox.Show("List Versioning Enabled");
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+
+        /// <summary>
+        /// Method to enable minor versioning 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button12_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Add List Description
+                using (ClientContext clientContext = new ClientContext("http://vm-021-tzc02/sites/CSOMSite/"))
+                {
+                    Web web = clientContext.Web;
+                    List list = web.Lists.GetByTitle("Employee");
+                    list.EnableMinorVersions = true;
+                    list.Update();
+                    clientContext.ExecuteQuery();
+                }
+                MessageBox.Show("List Minor Versioning Enabled");
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Method to enable Major versioning
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button13_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Add List Description
+                using (ClientContext clientContext = new ClientContext("http://vm-021-tzc02/sites/CSOMSite/"))
+                {
+                    Web web = clientContext.Web;
+                    List list = web.Lists.GetByTitle("Employee");
+                    list.EnableVersioning = true;
+                    list.MajorVersionLimit = 200;
+                    list.Update();
+                    clientContext.ExecuteQuery();
+                }
+                MessageBox.Show("List Major Versioning with limit Enabled");
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Method to enable force checkout
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button14_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Add List Description
+                using (ClientContext clientContext = new ClientContext("http://vm-021-tzc02/sites/CSOMSite/"))
+                {
+                    Web web = clientContext.Web;
+                    List list = web.Lists.GetByTitle("Employee");
+                    list.ForceCheckout = true;
+                    list.Update();
+                    clientContext.ExecuteQuery();
+                }
+                MessageBox.Show("Force CHeckout Enabled");
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Method to enable folder creation
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button15_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Add List Description
+                using (ClientContext clientContext = new ClientContext("http://vm-021-tzc02/sites/CSOMSite/"))
+                {
+                    Web web = clientContext.Web;
+                    List list = web.Lists.GetByTitle("Employee");
+                    list.EnableFolderCreation = true;
+                    list.Update();
+                    clientContext.ExecuteQuery();
+                }
+                MessageBox.Show("Folder CReation Enabled");
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// To enable quick launch
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button16_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Add List Description
+                using (ClientContext clientContext = new ClientContext("http://vm-021-tzc02/sites/CSOMSite/"))
+                {
+                    Web web = clientContext.Web;
+                    List list = web.Lists.GetByTitle("Employee");
+                    list.OnQuickLaunch = true;
+                    list.Update();
+                    clientContext.ExecuteQuery();
+                }
+                MessageBox.Show("Quick launch Enabled");
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// /TO anbale content types
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button17_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Add List Description
+                using (ClientContext clientContext = new ClientContext("http://vm-021-tzc02/sites/CSOMSite/"))
+                {
+                    Web web = clientContext.Web;
+                    List list = web.Lists.GetByTitle("Employee");
+                    list.ContentTypesEnabled = true;
+                    list.Update();
+                    clientContext.ExecuteQuery();
+                }
+                MessageBox.Show("Content types Enabled");
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (ClientContext clientContext = new ClientContext("http://vm-021-tzc02/sites/CSOMSite/"))
+                {
+                    Web web = clientContext.Web;
+                    List list = web.Lists.GetByTitle("Employee");
+                    clientContext.Load(list);
+                    clientContext.ExecuteQuery();
+                    ContentTypeCollection contentTypes = clientContext.Site.RootWeb.ContentTypes;
+                    clientContext.Load(contentTypes);
+                    clientContext.ExecuteQuery();
+
+                    ContentType contentType = contentTypes.Where(c => c.Name == "Timecard").First();
+                    list.ContentTypes.AddExistingContentType(contentType);
+                    list.Update();
+                    clientContext.ExecuteQuery();
+                }
+                MessageBox.Show("Content types Added");
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (ClientContext clientContext = new ClientContext("http://vm-021-tzc02/sites/CSOMSite/"))
+                {
+                    Web web = clientContext.Web;
+                    List list = web.Lists.GetByTitle("Employee");
+                    clientContext.Load(list);
+                    clientContext.Load(list.ContentTypes);
+                    clientContext.ExecuteQuery();
+                    ContentTypeCollection contentTypes = list.ContentTypes;
+                    clientContext.Load(contentTypes);
+                    clientContext.ExecuteQuery();
+
+                    ContentType contentType = contentTypes.Where(c => c.Name == "Timecard").First();
+                    contentType.DeleteObject();
+                    list.Update();
+                    clientContext.ExecuteQuery();
+                }
+                MessageBox.Show("Content type removed from the list");
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
        
+
+        private void button20_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                using (ClientContext clientContext = new ClientContext("http://vm-021-tzc02/sites/CSOMSite/"))
+                {
+                    Web web = clientContext.Web;
+                    List list = web.Lists.GetByTitle("Employee");
+                    clientContext.Load(list);
+                    clientContext.Load(list.ContentTypes);
+                    clientContext.ExecuteQuery();
+                    ContentTypeCollection contentTypes = list.ContentTypes;
+                    clientContext.Load(contentTypes);
+                    clientContext.ExecuteQuery();
+
+                    for (int i = 0; i < contentTypes.Count; i++)
+                    {
+                        ContentType contentType = contentTypes[i];
+                        contentType.DeleteObject();
+                    }
+
+                    list.Update();
+                    clientContext.ExecuteQuery();
+                }
+                MessageBox.Show("All Content type removed from the list");
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
     }
 }
